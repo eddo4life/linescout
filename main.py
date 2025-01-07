3a18ac548631cb0595f70fb352f435f929c53214
eddo4life <theoboaz@gmail.com>
2025-01-07 10:27:05 -0500

    Treat as extension if arg starts with a dot

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
+import argparse
 from file_utils import count_files_and_lines_by_extension
 from summary import generate_summary
-import argparse
-from config import DEFAULT_EXTENSION,DEFAULT_DIRECTORY
+from config import DEFAULT_EXTENSION, DEFAULT_DIRECTORY
 
 def main():
     parser = argparse.ArgumentParser(description="LineScout: Scan directories for file line counts.")
-    parser.add_argument("directory",nargs="?",default=DEFAULT_DIRECTORY, help="Directory to scan: Default is the current working directory.")
-    parser.add_argument("extension", nargs="?", default=DEFAULT_EXTENSION, help="File extension to filter by (e.g., .txt, .py). Defaults to config value.")
-    parser.add_argument("--size", action="store_true", help="Include file size in summary")
-    parser.add_argument("--report", action="store_true", help="Generate a detailed report")
-    
+    parser.add_argument("directory",nargs="?",default=DEFAULT_DIRECTORY,help="Directory to scan: Default is the current working directory.")
+    parser.add_argument("extension",nargs="?",default=DEFAULT_EXTENSION,help="File extension to filter by (e.g., .txt, .py). Defaults to config value.")
+    parser.add_argument("--size", action="store_true", help="Include file size in summary.")
+    parser.add_argument("--report", action="store_true", help="Generate a detailed report.")
+
     args = parser.parse_args()
+
+    if args.directory.startswith('.'):
+    # Treat as extension if it starts with a dot
+        args.extension = args.directory
+        args.directory = DEFAULT_DIRECTORY
+
 
     # Count files and lines
     file_count, total_lines = count_files_and_lines_by_extension(args.directory, args.extension)
-    
+
     # Print the summary
     print(f"The number of files with extension '{args.extension}' is: {file_count}")
     print(f"The total number of lines in those files is: {total_lines}")
-    
-    # Optionally, print file size and generate detailed report
+
+    # Optionally, print file size and generate a detailed report
     if args.size:
         print("Including file sizes in summary...")
-    
+
     if args.report:
         generate_summary(args.directory, args.extension)
 
